93e1d32b32fc738aab4632ebd318d0cd4bc80d67
parth-283 <kp2832002@gmail.com>
2022-02-18 17:07:14 +0530

    add mysql package and create database in phpmyadmin and show data

---
+++ node/mysqlconnect.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"test"
+});
+con.connect(function(error){
+    if(error) throw error;
+    con.query("select * from test1",function(err,result){
+        if(err)throw err;
+        console.warn("all results are here",result)
+    })
+})
