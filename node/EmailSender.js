57a304494aba921d353d311b43acf80f6eb115f7
parth-283 <kp2832002@gmail.com>
2022-02-18 12:19:38 +0530

    add nodemailer package and it's use

---
+++ node/EmailSender.js
@@ -0,0 +1,24 @@
+var nodeMailer = require("nodemailer");
+var transport = nodeMailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: "kathiriyaparth283@gmail.com",
+    pass: "parth@2002",
+  },
+});
+var mailOptions = {
+  from: "kathiriyaparth283@gmail.com",
+  to: "kathiriyaparth283@gmail.com",
+  subject: "test nodemailer",
+  text: "hii, how are you!!!",
+};
+transport.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.warn(error);
+  } else {
+    console.warn("email has been sent", info.response);
+  }
+});
