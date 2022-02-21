ec3a7ec00f18a883d14bf62b9ee37ab8944caee1
parth-283 <kp2832002@gmail.com>
2022-02-21 12:24:59 +0530

    express routing and open file with router

--- node/expressApp.js
+++ node/expressApp.js
@@ -4,13 +4,18 @@ const router = express.Router();
 const urlcheck = require("./middleware");
 
 app.get("/", function (req, res) {
-  res.send("hello express");
+//   res.send("hello express");
+res.sendfile(__dirname+'/Home.html')
 });
 router.get("/about", urlcheck, function (req, res) {
-  res.send("hello about");
+//   res.send("hello about");
+res.sendfile(__dirname+'/About.html')
+
 });
 app.get("/login", function (req, res) {
-  res.send("hello login");
+//   res.send("hello login");
+res.sendfile(__dirname+'/Login.html')
+
 });
 app.use("/", router);
 app.listen(4000);
